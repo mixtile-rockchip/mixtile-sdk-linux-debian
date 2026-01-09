a93acff0c89a52901269ee73e159734f56740567
Tom <1259233520@qq.com>
2026-01-09 10:05:55 +0800

    Comment out Chinese fonts installation and locale setup
    Comment out installation of Chinese fonts and related locale settings.

--- mk-rootfs-bookworm.sh
+++ mk-rootfs-bookworm.sh
@@ -159,19 +159,19 @@ fi
 echo -e "\033[36m Install rktoolkit.................... \033[0m"
 \${APT_INSTALL} /packages/rktoolkit/*.deb
 
-echo -e "\033[36m Install Chinese fonts.................... \033[0m"
+#echo -e "\033[36m Install Chinese fonts.................... \033[0m"
 # Uncomment zh_CN.UTF-8 for inclusion in generation
-sed -i 's/^# *\(zh_CN.UTF-8\)/\1/' /etc/locale.gen
-echo "LANG=zh_CN.UTF-8" >> /etc/default/locale
+#sed -i 's/^# *\(zh_CN.UTF-8\)/\1/' /etc/locale.gen
+#echo "LANG=zh_CN.UTF-8" >> /etc/default/locale
 
 # Generate locale
-locale-gen
+#locale-gen
 
-\${APT_INSTALL} ttf-wqy-zenhei fonts-aenigma
-\${APT_INSTALL} xfonts-intl-chinese
+#\${APT_INSTALL} ttf-wqy-zenhei fonts-aenigma
+#\${APT_INSTALL} xfonts-intl-chinese
 
 #------------------ibus------------
-\${APT_INSTALL} ibus ibus-libpinyin
+#\${APT_INSTALL} ibus ibus-libpinyin
 
 #------------------pipewire------------
 echo -e "\033[36m Install pipewire.................... \033[0m"
